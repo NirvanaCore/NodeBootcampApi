2184f0541283da5b3f37a0f36de7a9034277cf81
NirvanaCore <nirvanacore7@gmail.com>
2021-01-14 00:24:38 +0530

    Database connected: MongoDb atlas

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //initialization of logger
 const morgan = require('morgan');
 //custom morgan
 //const logger = require('./middleware/logger');
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
-
 //load config env files
 dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -18,7 +22,7 @@ const app = express();
 // app.use(logger);
 
 //dev logging middleware
-if ((process.env.NODE_ENV === 'development')) {
+if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
@@ -26,7 +30,17 @@ if ((process.env.NODE_ENV === 'development')) {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.NODE_ENV} mode on  port ${PORT}`)
 );
+
+//Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(` Error : ${err.message}`);
+	//close server and exit process
+	server.close(() => {
+		process.exit(1);
+	});
+});
