dea4b40b1fa18cdcabc08cc57e19fe5c0eba6b1b
NirvanaCore <nirvanacore7@gmail.com>
2021-01-13 18:48:46 +0530

    MiddleWare set up

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//initialization of logger
+const morgan = require('morgan');
+//custom morgan
+//const logger = require('./middleware/logger');
+
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
@@ -9,15 +14,13 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-const logger = (req, res, next) => {
-	req.hello = 'Hello Soniya';
-	console.log(
-		`${req.method} ${req.protocol} ://${req.get('host')} ${req.originalUrl}`
-	);
-	next();
-};
+// logger from middleware
+// app.use(logger);
 
-app.use(logger);
+//dev logging middleware
+if ((process.env.NODE_ENV === 'development')) {
+	app.use(morgan('dev'));
+}
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
